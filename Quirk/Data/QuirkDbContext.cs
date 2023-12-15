5880d6b573363e37aee7bc734f14a91bc6b6d482
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-12-15 12:28:54 +0100

    CreatedDbContext

---
+++ Quirk/Data/QuirkDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Quirk.Models.Domain;
+
+namespace Quirk.Data
+{
+    public class QuirkDbContext : DbContext
+    {
+        public QuirkDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
