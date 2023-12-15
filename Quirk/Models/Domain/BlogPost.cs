a889821d09b51675e83bd89b3e7f6535f8da5997
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-12-15 10:47:17 +0100

    CreatedBlogTagModel

--- Quirk/Models/Domain/BlogPost.cs
+++ Quirk/Models/Domain/BlogPost.cs
@@ -2,5 +2,16 @@
 {
     public class BlogPost
     {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; } = DateTime.UtcNow;
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+        public ICollection<Tag> Tags { get; set; } 
     }
 }
---
+++ Quirk/Models/Domain/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace Quirk.Models.Domain
+{
+    public class Tag
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
