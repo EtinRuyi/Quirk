58c96819e6ace167521bab6e46e1d89af9d5c05a
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-12-16 02:06:44 +0100

    AddBlogPost

--- Quirk/Controllers/AdminBlogPostController.cs
+++ Quirk/Controllers/AdminBlogPostController.cs
@@ -1,13 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Quirk.Models.ViewModels;
+using Quirk.Repositories;
 
 namespace Quirk.Controllers
 {
     public class AdminBlogPostController : Controller
     {
-        [HttpGet]
-        public IActionResult Add()
+        private readonly ITagRepository _tagRepository;
+        public AdminBlogPostController(ITagRepository tagRepository)
         {
-            return View();
+            _tagRepository = tagRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var tags = await _tagRepository.GetAllAsync();
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString()})
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest)
+        {
+            return RedirectToAction("Add");
         }
     }
 }
