b0e5f1fdaa05c9a199a5b6989c208b58fcbc1a1a
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-12-16 01:10:07 +0100

    AddBlogPost

---
+++ Quirk/Controllers/AdminBlogPostController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Quirk.Controllers
+{
+    public class AdminBlogPostController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
