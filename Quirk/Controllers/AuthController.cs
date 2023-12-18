1b3ccf389df6b0073612f507bdd1b65a3c1a2e15
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-12-18 13:18:56 +0100

    RegisterUser

---
+++ Quirk/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Quirk.Models.ViewModels;
+
+namespace Quirk.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            var user = new IdentityUser
+            {
+                UserName = registerViewModel.UserName,
+                Email = registerViewModel.Email,
+            };
+            var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+            if (result.Succeeded)
+            {
+                var role = await _userManager.AddToRoleAsync(user, "User");
+                if (role.Succeeded)
+                {
+                    return RedirectToAction("Register");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Quirk/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Quirk.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [DisplayName("User Name")]
+        public string UserName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
