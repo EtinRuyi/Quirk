8777ef9d87f60517b0209bcd278713d5842e691e
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-12-30 23:07:42 +0100

    InterfaceUpdated

--- Quirk/wwwroot/js/site.js
+++ Quirk/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//<script>
+//    // Wait for the document to be ready
+//    $(document).ready(function () {
+//        // Cache the navbar element
+//        var navbar = $('.navbar');
+
+//    // Check the initial scroll position
+//    updateNavbarStyle($(window).scrollTop());
+
+//    // Attach a scroll event listener
+//    $(window).scroll(function () {
+//        // Update the navbar style on scroll
+//        updateNavbarStyle($(window).scrollTop());
+//        });
+
+//    // Function to update the navbar style based on the scroll position
+//    function updateNavbarStyle(scrollTop) {
+//            // Define the threshold for changing the navbar style
+//            var scrollThreshold = 50;
+
+//            // Check if the scroll position is beyond the threshold
+//            if (scrollTop > scrollThreshold) {
+//        // Add the scrolled class to the navbar
+//        navbar.addClass('navbar-scrolled');
+//            } else {
+//        // Remove the scrolled class from the navbar
+//        navbar.removeClass('navbar-scrolled');
+//            }
+//        }
+//    });
+//</script>
